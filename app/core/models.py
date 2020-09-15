03fe75622bb2337519e2e761dc8a6ec07762721d
unknown <carlos.alonsogtz@hotmail.es>
2020-09-15 09:19:17 +0200

    Added Recipe Model

--- app/core/models.py
+++ app/core/models.py
@@ -65,3 +65,21 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    # Blank is better because, if required,
+    # you just check whether it's blank :)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
